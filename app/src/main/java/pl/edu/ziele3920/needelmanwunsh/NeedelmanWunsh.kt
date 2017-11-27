582939bfdbc298f594f04a9dee13b8266d9b64a6
ziele3920 <a.ziolkowski3920@wp.pl>
2017-11-27 22:48:51 +0100

    path memo

--- app/src/main/java/pl/edu/ziele3920/needelmanwunsh/NeedelmanWunsh.kt
+++ app/src/main/java/pl/edu/ziele3920/needelmanwunsh/NeedelmanWunsh.kt
@@ -23,28 +23,45 @@ class NeedelmanWunsh(
         this.mismatchDiagonalPunish = mismatchDiagonalPunish ?: this.defaultMismatchDiagonalPunish
     }
 
-
     private lateinit var resultTable: Array<IntArray>
     private lateinit var resultSequences: Array<String>
+    private lateinit var paths: Array<Array<MutableList<Pair<Pair<Int, Int>, Pair<String, String>>>>>
+    private lateinit var alignments: MutableList<Pair<String, String>>
     private var score: Int = 0
 
     fun getResultStringArray(): Array<Array<String>> {
         resultTable = Array(vericalSeq.length+1, {IntArray(horizontalSeq.length+1)})
+        paths = Array(vericalSeq.length+1, { Array(horizontalSeq.length+1, { mutableListOf<Pair<Pair<Int, Int>, Pair<String, String>>>()}) })
 
-
-        for (i in 0..vericalSeq.length)
+        for (i in 0..vericalSeq.length) {
             resultTable[i][0] = -i
+            if(i > 0)
+                paths[i][0].add(Pair(Pair(i-1, 0), Pair("", "")))
+        }
 
-        for (i in 0..horizontalSeq.length)
+        for (i in 0..horizontalSeq.length) {
             resultTable[0][i] = -i
+            if(i > 0)
+                paths[i][0].add(Pair(Pair(0, i-1), Pair("", "")))
+        }
 
         for (i in 1..vericalSeq.length) {
             for (j in 1..horizontalSeq.length) {
-                if (vericalSeq[i-1] == horizontalSeq[j-1])
-                    resultTable[i][j] = resultTable[i - 1][j - 1] + reward
-                else
-                    resultTable[i][j] = resultTable[i - 1][j - 1] + mismatchDiagonalPunish
-                    resultTable[i][j] = Math.max(Math.max(resultTable[i - 1][j], resultTable[i][j - 1]) + indelPunish, resultTable[i][j])
+                val diagonalPathScore = resultTable[i - 1][j - 1] + if (vericalSeq[i-1] == horizontalSeq[j-1]) reward else mismatchDiagonalPunish
+                val verticalPathScore = resultTable[i - 1][j] + indelPunish
+                val horizontalPathScore = resultTable[i][j - 1] + indelPunish
+                val max = Math.max(diagonalPathScore, Math.max(verticalPathScore, horizontalPathScore))
+                resultTable[i][j] = max
+                var d =7
+                val c = 4
+                if(max == 3)
+                    d +=c
+                if(diagonalPathScore == max)
+                    paths[i][j].add(Pair(Pair(i-1, j-1), Pair(vericalSeq[i-1].toString(), horizontalSeq[j-1].toString())))
+                if(verticalPathScore == max)
+                    paths[i][j].add(Pair(Pair(i-1, j), Pair(vericalSeq[i-1].toString(), "_")))
+                if(horizontalPathScore == max)
+                    paths[i][j].add(Pair(Pair(i, j-1), Pair("_", horizontalSeq[j-1].toString())))
             }
         }
         calculateResults()
@@ -57,6 +74,44 @@ class NeedelmanWunsh(
 
     fun getScore(): Int{
         return score
+    }
+
+    private fun generateAlignments() {
+        alignments = mutableListOf()
+        var max = Int.MIN_VALUE
+        var maxX = 0
+        var maxY = 0
+        for(i in 0 until resultTable[0].size) {
+            if (resultTable[resultTable.size - 1][i] >= max) {
+                max = resultTable[resultTable.size - 1][i]
+                maxX = resultTable.size - 1
+                maxY = i
+            }
+        }
+        for(i in 0 until resultTable.size) {
+            if (resultTable[i][resultTable[0].size-1] >= max) {
+                max = resultTable[i][resultTable[0].size-1]
+                maxX = i
+                maxY = resultTable[0].size-1
+            }
+        }
+        for(stepCoord in paths[maxX][maxY]) {
+            var verticalStringAlignment = StringBuilder(stepCoord.second.first)
+            var horizontalStringAlignment = StringBuilder(stepCoord.second.second)
+            stepPath(horizontalStringAlignment, verticalStringAlignment, stepCoord.first)
+        }
+    }
+
+    private fun stepPath(horizontalStringAlignment: StringBuilder, verticalStringAlignment: StringBuilder, destinationCoords: Pair<Int, Int>) {
+        horizontalStringAlignment.append(horizontalSeq[destinationCoords.second-1])
+        verticalStringAlignment.append(vericalSeq[destinationCoords.first-1])
+        if(paths[destinationCoords.first][destinationCoords.second].size == 0) {
+            alignments.add(Pair(horizontalStringAlignment.toString(), verticalStringAlignment.toString()))
+            return
+        }
+        //for(stepCoord in paths[destinationCoords.first][destinationCoords.second]) {
+        //    if(horizontalSeq[sourceCoords.second-1] == )
+        //}
     }
 
     private fun calculateResults() {
